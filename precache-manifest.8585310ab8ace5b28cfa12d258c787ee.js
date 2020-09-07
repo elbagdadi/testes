38f55ee99812f72e7bb1c531363aeb6950ac28ed
Abderrahim EL BAGDADI <elbagdadi.abdou@gmail.com>
2020-09-07 14:13:05 +0100

    Updates

---
+++ precache-manifest.8585310ab8ace5b28cfa12d258c787ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "734c00cd85a6ddf255a826819bf3a249",
+    "url": "/testes/index.html"
+  },
+  {
+    "revision": "ba9093511419d79b872a",
+    "url": "/testes/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1d58aa4e10b3c76d11f5",
+    "url": "/testes/static/js/2.d074e3ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/testes/static/js/2.d074e3ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba9093511419d79b872a",
+    "url": "/testes/static/js/main.9795a629.chunk.js"
+  },
+  {
+    "revision": "629a8fe1d571fa50dc1d",
+    "url": "/testes/static/js/runtime-main.2b6c9031.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testes/static/media/logo.5d5d9eef.svg"
+  }
+]);
